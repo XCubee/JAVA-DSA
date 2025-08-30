d4df1cbd723d7b6df765089006fa7af64ccdb373
Saksham Ahluwalia <63649583+XCubee@users.noreply.github.com>
2025-08-30 17:32:49 +0530

    Create Max_Subarray_Sum.java

---
+++ Max_Subarray_Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int ms = nums[0];
+        int cs = nums[0];
+        int min = nums[0]; 
+
+        for (int i = 1; i < nums.length; i++) {
+            cs = Math.max(nums[i], cs + nums[i]); 
+            ms = Math.max(ms, cs);
+            min = Math.min(min, nums[i]); // update min if needed
+        }
+
+        return ms; 
+    }
+}
