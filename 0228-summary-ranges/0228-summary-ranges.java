45540b12a10cecdd3a275bc1b7c1933f1fab4672
Saksham Ahluwalia <63649583+XCubee@users.noreply.github.com>
2026-07-07 12:30:38 +0530

    Time: 3 ms (79.41%), Space: 43 MB (53.85%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -2,23 +2,29 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
         int n = nums.length;
-        int i=0;
-        while(i<n){
-            int start = nums[i];
-
-            while(i+1<n && nums[i+1]==nums[i]+1){
-                i++;
-            }
-            if (start==nums[i]){
-                result.add(String.valueOf(start));
-            }
-            else{
-                result.add(start+"->"+nums[i]);
-            }
-             i++;
+        if (n == 0) {
+            return result;
         }
-       
-        
+
+        int start = nums[0];
+        for (int i = 1; i < n; i++) {
+            // Boundary: previous element closed a range
+            if (nums[i] != nums[i - 1] + 1) {
+                addRange(result, start, nums[i - 1]);
+                start = nums[i];
+            }
+        }
+        // Final open range ends at the last element
+        addRange(result, start, nums[n - 1]);
+
         return result;
+    }
+
+    private void addRange(List<String> result, int start, int end) {
+        if (start == end) {
+            result.add(String.valueOf(start));
+        } else {
+            result.add(start + "->" + end);
+        }
     }
 }
