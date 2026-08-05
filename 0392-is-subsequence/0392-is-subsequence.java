921d7fd3c05d0ed64877dd749d5fb5dc0a6a0db7
Saksham Ahluwalia <63649583+XCubee@users.noreply.github.com>
2026-08-05 20:52:26 +0530

    Time: 3 ms (3.62%), Space: 45.9 MB (11.29%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,15 +1,38 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int j=0;
-        if(s.length()==0) return true;
-        for(int i=0;i<t.length();i++){
-            if(t.charAt(i)==s.charAt(j)){
-                j++;
-                if(j==s.length()){
-                    return true;
-                }
-            }
+        int n = t.length();
+        int m = s.length();
+        
+        // dp[i][ch] = index of next occurrence of 'ch' at or after index i in t
+        int[][] dp = new int[n + 1][26];
+        
+        // Base case: past the end of string t, no characters exist
+        for (int c = 0; c < 26; c++) {
+            dp[n][c] = -1;
         }
-return false;
+        
+        // Build DP matrix from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            for (int c = 0; c < 26; c++) {
+                dp[i][c] = dp[i + 1][c];
+            }
+            dp[i][t.charAt(i) - 'a'] = i;
+        }
+        
+        // Query string s using the precomputed matrix
+        int currPos = 0;
+        for (int i = 0; i < m; i++) {
+            int charIdx = s.charAt(i) - 'a';
+            
+            // If character does not exist at or after current position
+            if (dp[currPos][charIdx] == -1) {
+                return false;
+            }
+            
+            // Jump directly to the next position after the match
+            currPos = dp[currPos][charIdx] + 1;
+        }
+        
+        return true;
     }
 }
